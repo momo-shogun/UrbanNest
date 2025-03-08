51ea5b377354bf3b797fc5ce1e293cc5365f3d00
momo-shogun <krishnafinger12345@gmail.com>
2025-03-09 01:42:12 +0530

    init

---
+++ app/SignIn.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+
+const SignIn = () => {
+  const handleLogin = () => {
+    console.log("Login");
+  };
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase text-black-200 font-rubik ">
+            Welcome To GharDeko
+          </Text>
+          <Text className="text-3xl font-rubikBold text-black-300 mt-2 text-center">
+            Lets get Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-center font-rubik text-black-200 mt-8">
+              Login to Ghar Deko With Google
+            </Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-400 rounded-full w-full py-4 mt-5 "
+          >
+            <View className="flex flex-row justify-center items-center gap-4"> 
+            <Image
+              source={icons.google}
+              className="w-5 h-5"
+              resizeMode="contain"
+            />
+            <Text className="text-center font-rubik text-black-200 font-bold">
+              Continue with Google 
+            </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
